5d9804509ae9f3bf2075d5c0c380adc57c5f2d93
btimlake <ben@bentimberlake.com>
2016-09-05 23:09:31 +0200

    Please wait notice
    Add please wait screen

--- games.m
+++ games.m
@@ -1,4 +1,4 @@
-function [gamesdatafilename]=games(subNo, anni, w, wRect, NUMROUNDS, enabledKeys)
+function [gamesdatafilename]=games(subNo, anni, w, wRect, NUMROUNDS, enabledKeys, cfg)
 % games(participant number, date/time, window size, window rectangle size,
 % number of rounds)
 
@@ -757,6 +757,23 @@ catch
     
     % Output the error message that describes the error:
     psychrethrow(psychlasterror);
+    
+    %% Screen 4a - Please wait; hold on result
+
+for i = 1:16; % multiply second integer by .75 to get seconds; i.e. 16 means 12 seconds
+      
+    if mod(i,2) == 0 %Even numbers: wait text on
+        % Please Wait text
+        DrawFormattedText(window, 'Si prega di attendere un attimo Le Ruote della Fortuna ? ', 'center', cfg.waitTextYpos, cfg.instColA);
+        Screen('Flip', window)
+        WaitSecs(1);
+    else %Odd numbers: wait text off
+        Screen('Flip', window)
+        WaitSecs(.5);
+    end
+    
+end
 end % try ... catch %
+end
 
 % RestrictKeysForKbCheck([]); % re-recognize all key presses
