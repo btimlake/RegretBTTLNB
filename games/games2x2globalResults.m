3ffd76dfe4edb5b50ea1b73e53f02aa16b9245b1
btimlake <ben@bentimberlake.com>
2016-12-06 17:28:54 +0100

    If/Switch changed to table/array access
    Updated dataset and table so they can be accessed more logically in the
    global results code (changed a nested if/switch statement to a simple
    matrix access based on variables).

--- games/games2x2globalResults.m
+++ games/games2x2globalResults.m
@@ -1,23 +1,28 @@
 function games2x2globalResults
+% Accesses files in given directory, randomly chooses one game from among 48, 
+% uses data to create matchups among all available files and outputs a dated 
+% file for use by each user running Remote, which produces a readout of the 
+% results, including game, image, adversary, player choice, adversary
+% choice and winnings.
+% Written in December 2016 by Ben Timberlake, Learning and Decision Making 
+% Group, University of Trento
 
+%% Files
+% Set file location
 % Make sure that only files from this session are in this directory
 addpath('RegretBTTLNB', 'RegretBTTLNB/games', 'RegretBTTLNB/games/stimoli', 'Subject data/02122016pm-games/');
-
-
 subject_data_dir = dir('Subject data/02122016pm-games/');
 subject_data = subject_data_dir(4:end); % gets all files and folders in the directory (after three invisible files)
-priming_effect_table = [];
-
 
 % Determine which game will be played
 chosenGame = randi(48,1,1);
-% chosenGame = 45;
 
+% Get reference file for which row game matches with which column game
 load('RegretBTTLNB/games/matching_responsesTABLE'); % becomes variable matchingresponses - MAC
 % load('RegretBTTLNB/games/matching_responsesDATASET'); % becomes variable matchingresponses - PC
 matchedGame = find(matchingresponses.column == chosenGame); % get the corresponding game from standard list
 
-games2x2files = dir('Subject data/02122016pm-games/*1games2x2.dat'); % get all the instances of 9all saved files
+games2x2files = dir('Subject data/02122016pm-games/*1games2x2.dat'); % get all the instances of games files
 
 % Check that there are an even number of files for the matchups
 if mod(length(games2x2files),2) == 0
@@ -34,6 +39,8 @@ for i=1:length(games2x2files)
     % get the choice from the chosen game
     chosenGameChoice = gamesData.Var5(global2x2.chosenTrial(i));
     matchedGameChoice = gamesData.Var5(global2x2.matchedTrial(i));
+    % Convert to numbers the choice for player choice and matched choice
+    % (matched choice will be provided to opponent)
     if strcmp(chosenGameChoice, 'UpArrow') == 1
         global2x2.numberChoice(i,:) = 1;
     elseif strcmp(chosenGameChoice, 'DownArrow') == 1
@@ -41,20 +48,24 @@ for i=1:length(games2x2files)
     else
         disp('Something bad happened with global2x2.chosenTrial')
     end
-    
+    % These numbers are opposite for player choice because they are played 
+    % as row games, but must be converted as if they were played as
+    % columns.
     if strcmp(matchedGameChoice, 'UpArrow') == 1
-        global2x2.numberMatch(i,:) = 2; %
+        global2x2.numberMatch(i,:) = 2; 
     elseif strcmp(matchedGameChoice, 'DownArrow') == 1
         global2x2.numberMatch(i,:) = 1;
     else
         disp('Something bad happened with global2x2.chosenTrial')
     end
-    
-%     tempString = gamesData.Var1(1); 
-%     global2x2.compNum(i,:) = str2num(tempString(5:6)); % Set computer name for easier matching
-        % Turn this one on for new string-based participant number
-        tempString = char(gamesData.Var1(1));
-        global2x2.compNum(i,:) = str2num(tempString(7:8)); % Set computer name for easier matching 
+
+%   Set the computer number for each row, based on participant number
+%   For previous, 5-digit participant numbers
+%   tempString = gamesData.Var1(1); 
+%   global2x2.compNum(i,:) = str2num(tempString(5:6)); % Set computer name for easier matching
+%   For 7-character, string-based participant number
+    tempString = char(gamesData.Var1(1));
+    global2x2.compNum(i,:) = str2num(tempString(7:8)); % Set computer name for easier matching 
 
 end
 
@@ -62,79 +73,21 @@ end
 matchPositions = global2x2.compNum(randperm(size(global2x2.compNum,1)),:);
 matchPositions = [matchPositions flipud(matchPositions)];
 
-% global2x2.opponent = global2x2.matchPositions(:,2); % sort of an extra step to simplify for loop
-
 for i = 1:length(games2x2files)
-
-%     global2x2.opponentPos = find(global2x2.opponent(i) == global2x2.compNum);
-%     global2x2.opponentChoice(i) = global2x2.numberMatch(global2x2.opponent(i) == global2x2.compNum);
-%     global2x2.opponentChoice(i) = global2x2.numberMatch(global2x2.opponent(i));
-
+    % figure out which row belongs to the opponent, then what the opponents
+    % match choice is; becomes a given player's opponent choice
     matchRow = find(matchPositions(:,1) == i);
     global2x2.opponent(i,:) = matchPositions(matchRow,2);
     global2x2.opponentChoice(i,:) = global2x2.numberMatch(global2x2.opponent(i,:));
 
-    switch global2x2.numberChoice(i)
-        case 1
-%             disp('case1')
-            if global2x2.opponentChoice(i) == 1
-                global2x2.winnings2x2(i,:) = matchingresponses.r_c11(chosenGame);
-%                 disp('r_c11')
-            else
-                global2x2.winnings2x2(i,:) = matchingresponses.r_c12(chosenGame);
-%                 disp('r_c12')
-            end
-        case 2
-%             disp('case2')
-            if global2x2.opponentChoice(i) == 1
-                global2x2.winnings2x2(i,:) = matchingresponses.r_c21(chosenGame);
-%                 disp('r_c21')
-            else
-                global2x2.winnings2x2(i,:) = matchingresponses.r_c22(chosenGame);
-%                 disp('r_c22')
-            end
-    end
+    % for TABLE, winnings stored as matchingresponses.r_c(game, player choice, opponent choice)
+    global2x2.winnings2x2(i,:) = matchingresponses.r_c(chosenGame, global2x2.numberChoice(i), global2x2.opponentChoice(i));
+    % for DATASET winnings stored as matchingresponses{game,9}(:, player choice, opponent choice)
+    % global2x2.winnings2x2(i,:) = matchingresponses{chosenGame,9}(:, global2x2.numberChoice(i), global2x2.opponentChoice(i));
 
 end    
-%     type           row    column    r_c22    r_c21    r_c11    r_c12    row_column
-%     'DSS_1'         2     10        7        5        4        6        ''        
-%     
-%     chosenGame = 2; 
-%     global2x2.numberChoice = 2;
-%     global2x2.opponentChoice = 1;
-%     switch global2x2.numberChoice
-%         case 1
-%             if global2x2.opponentChoice == 1
-%                 global2x2.winnings2x2 = matchingresponsesDATASET.r_c11(chosenGame);
-%             else
-%                 global2x2.winnings2x2 = matchingresponsesDATASET.r_c12(chosenGame);
-%             end
-%         case 2
-%             if global2x2.opponentChoice == 1
-%                 global2x2.winnings2x2 = matchingresponsesDATASET.r_c21(chosenGame);
-%             else
-%                 global2x2.winnings2x2 = matchingresponsesDATASET.r_c22(chosenGame);
-%             end
-%     end    
-%     global2x2.winnings2x2
-%     
-    % Needed for PC?
-%     switch global2x2.numberChoice
-%         case 1
-%             if global2x2.opponentChoice == 1
-%                 global2x2.winnings2x2 = matchingresponses.r_c11(chosenGame);
-%             else
-%                 global2x2.winnings2x2 = matchingresponses.r_c12(chosenGame);
-%             end
-%         case 2
-%             if global2x2.opponentChoice == 1
-%                 global2x2.winnings2x2 = matchingresponses.r_c21(chosenGame);
-%             else
-%                 global2x2.winnings2x2 = matchingresponses.r_c22(chosenGame);
-%             end
-%     end
 
-
+% Determines filename for results based on morning/afternoon session
 if str2num(datestr(now,'HHMM')) < 1400
     save(['games', datestr(now,'ddmm'), 'am.mat'], 'chosenGame', 'global2x2')
 else
--- games/games2x2winningsRemote.m
+++ games/games2x2winningsRemote.m
@@ -1,57 +1,9 @@
 function [winnings2x2, chosenGame2x2, opponentChoice2x2] = games2x2winningsRemote(compNum, cfg, window)
-
-% %% Developing/debugging material
-% addpath('REGRET_task', 'patentTaskBTMP', 'ratingslider', 'instructions', 'games', 'games/stimoli');
-% 
-% PsychDefaultSetup(2);
-% screens = Screen('Screens');
-% screenNumber = 1;
-% white = WhiteIndex(screenNumber);
-% black = BlackIndex(screenNumber);
-% % [window, windowRect] = PsychImaging('OpenWindow', screenNumber, white, [0 0 640 480]); % for one screen setup 
-% [window, windowRect] = PsychImaging('OpenWindow', screenNumber, white); % for two-screen setup
-% [xCenter, yCenter] = RectCenter(windowRect);
-%  
-% particNum = '1212am02'; 
-% DateTime = '0101-1235'; 
-% enabledKeys = RestrictKeysForKbCheck([30, 44, 79, 80, 81,82]);
-% [screenXpixels, screenYpixels] = Screen('WindowSize', window);
-% 
-%     cfg.enabledSelectKeys = [44, 79, 80]; % limit recognized presses to space, left, right arrows MAC
-%     cfg.enabledExpandedKeys = [30, 34, 44, 79:82, 89, 93]; % limit recognized presses to 1!, 5%, space, left, right, up, down arrows, keypad1, keypad5 MAC
-%     cfg.limitedKeys = [79, 80]; % limit recognized presses to left, right arrows MAC
-%     cfg.enabledNumberKeys = [30:40, 55, 89:99,]; % limit recognized presses to 1-10, return, decimal, keypad 1-10 & decimal MAC
-% 
-% cfg.screenSize.x = screenXpixels; 
-% cfg.screenSize.y = screenYpixels;
-% cfg.font = 'Courier New';
-% cfg.fontSize = round(screenYpixels * 2/40);
-% % Colors
-% cfg.textColor = [0, 0, 0]; % black
-% % cfg.bgColor = [255, 255, 255];
-% cfg.bgColor = [1 1 1]; % white
-% cfg.instColA = [0, 0.4078, 0.5451]; %DeepSkyBlue4
-% cfg.instColB = [0.8039, 0.5843, 0.0471]; %DarkGoldenRod3
-% cfg.p1Col = [0, 0, 0.8039]; %MediumBlue
-% cfg.p2Col = [0.4314, 0.4824, 0.5451]; % LightSteelBlue4
-% cfg.winCol = [.1333, .5451, .1333]; %ForestGreen
-% % Positions
-% cfg.screenCenter = [xCenter, yCenter]; % center coordinatesf
-% cfg.topTextYpos = screenYpixels * 2/40; % Screen Y positions of top/instruction text
-% cfg.uppTextYpos = screenYpixels * 6/40;
-% cfg.midTextYpos = screenYpixels * 20/40;
-% cfg.botTextYpos = screenYpixels * 35/40; % Screen Y positions of bottom/result text
-% cfg.waitTextYpos = screenYpixels * 38/40; % Y position of lowest "Please Wait" text
-% cfg.LeftTextXpos = screenXpixels*.035;
-% 
-% cfg.fontSizeSmall = round(cfg.fontSize/2);
-% 
-%     Screen('TextFont', window, cfg.font);
-%     Screen('TextSize', window, cfg.fontSizeSmall);
-%     Screen('TextStyle', window);
-%     Screen('TextColor', window, cfg.textColor);
-% %     Screen('Preference', 'TextAlphaBlending', 1);
-% 	oldTextBackgroundColor=Screen('TextBackgroundColor', window, [255 255 255]); 
+% Accesses a dated file and produces a readout of individual results of  
+% 2x2games, including selected game, image, adversary, player choice, 
+% adversary choice and winnings.
+% Written in December 2016 by Ben Timberlake, Learning and Decision Making 
+% Group, University of Trento 
     
 %% load data
 if str2num(datestr(now,'HHMM')) < 1200
@@ -60,33 +12,17 @@ else
     gamesData = load(['games', datestr(now,'ddmm'), 'pm.mat']);
 end
 
-    % for 5-digit participant numbers
+% Find the row that corresponds to this user's information
+% for 5-digit participant numbers
     %     tempString = num2str(particNum); 
     %     thisCompNum = str2num(tempString(5:6)); % Set computer name for easier matching
-        % Turn this one on for new string-based participant number
-%         tempString = char(particNum);
-%         thisCompNum = str2num(tempString(7:8)); % Set computer name for easier matching 
-
+% for 7-digit, string-based participant number
+    %     tempString = char(particNum);
+    %     thisCompNum = str2num(tempString(7:8)); % Set computer name for easier matching 
+% for 2-digit computer number
         datarow = find(str2num(compNum) == gamesData.global2x2.compNum); % finds which row this user's data is stored on
 
-% from games file, save a file with simplified name
-% on the server, there has to be a script that collates all the submitters,
-   % pull all the games files into one server
-   % script that collates data into one six-column file; redistribute this
-   % to the 2 or three servers
-% randomizes them and then provides the information for this script
-% maybe into one file that they all go looking for named for date and
-% afternoon or morning session
-% run that script as soon as they do the one-shot?
-
-% use payouts script
-% have it display the game image
-% have it display the opponent's number
-% have it display the player's choice
-% have it dispaly the opponent's choice
-% info on single row: computer number, game, player choice, opponent,
-% opponent choice, winnings
-
+% Turn player choice into a number
     if gamesData.global2x2.numberChoice(datarow) == 1
         thisChoice = 'I';
     elseif gamesData.global2x2.numberChoice(datarow) == 2
@@ -94,7 +30,8 @@ end
     else
         disp('Something bad happened with chosenChoice')
     end
-    
+
+% Turn opponent choice into a number
     if gamesData.global2x2.opponentChoice(datarow) == 1
         oppChoice = 'i'; %
     elseif gamesData.global2x2.opponentChoice(datarow) == 2
@@ -103,17 +40,19 @@ end
         disp('Something bad happened with matchedChoice')
     end
     
+% save opponent choice, winnings and chosen game as variables
     opponentChoice2x2 = gamesData.global2x2.opponentChoice(datarow);
     winnings2x2 = gamesData.global2x2.winnings2x2(datarow);
     chosenGame2x2 = gamesData.chosenGame;
     
-payout2x2.label = {'Gioco',  'Avversario',  'Scelta tua',  'Scelta sua',  'Guadagno'}';
+% payout2x2.label = {'Gioco',  'Avversario',  'Scelta tua',  'Scelta sua',  'Guadagno'}';
 
+% Prepare text lines for readout
 text = {['Gioco: ', num2str(gamesData.chosenGame)] ...
     ['Avversario: ', num2str(gamesData.global2x2.opponent(datarow))] ...
-['Hai scelto: ', thisChoice] ...
- ['L''avversario ha scelto: ', oppChoice] ...
- ['Hai guadagnato: ', num2str(gamesData.global2x2.winnings2x2(datarow))]};
+    ['Hai scelto: ', thisChoice] ...
+    ['L''avversario ha scelto: ', oppChoice] ...
+    ['Hai guadagnato: ', num2str(gamesData.global2x2.winnings2x2(datarow))]};
 
 % Get image of chosen game
 trialfilename  = 'stimoli.txt';                            % Experimental list
@@ -122,19 +61,19 @@ stimfilename=strcat('stimoli/',char(objname(gamesData.chosenGame))); % assume st
 imdata=imread(char(stimfilename));
 tex=Screen('MakeTexture', window, imdata);
 imageRectPos = [cfg.screenCenter(1)-cfg.screenSize.x/5 cfg.topTextYpos+(2*cfg.fontSize) cfg.screenCenter(1)+cfg.screenSize.x/5 cfg.topTextYpos+(2*cfg.fontSize)+cfg.screenSize.y/2.5];
-% Screen('DrawTexture', window, tex, [], imageRectPos);
-    Screen('DrawTexture', window, tex, [], imageRectPos);
+Screen('DrawTexture', window, tex, [], imageRectPos);
 
+% Draw top line of text to screen
 DrawFormattedText(window, char(text(1)), 'center', cfg.topTextYpos); % Result text
 Screen('Flip', window)
-    WaitSecs(1.5);
+WaitSecs(1.5);
     
+% Make y positions for subsequent text lines
 for i = 2:length(text)
     yPos(i) = cfg.midTextYpos + i*cfg.fontSize; % sets Y position of each line of text
 end
-    
-nyi = cfg.midTextYpos;
-nyj = nyi+(cfg.fontSize);
+
+% Add text lines one by one
 for i = 2:length(text);
     Screen('DrawTexture', window, tex, [], imageRectPos);
     DrawFormattedText(window, char(text(i)), 'center', yPos(i)); % Result text
@@ -148,10 +87,13 @@ for i = 2:length(text);
     WaitSecs(1.5);
 end
 
+DrawFormattedText(window, 'Si prega di attendere un attimo i prossimi resultati ... ', 'center', cfg.topTextYpos, cfg.instColA); % Result text
+Screen('Flip', window, 0, 1)
+
+% Let result sit for 5 seconds
 WaitSecs(5);
 
 end
-
 
 
 
